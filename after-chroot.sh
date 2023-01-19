ed85567cbb0931ead9285293d361c2173df82380
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-20 09:25:07 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -5,3 +5,7 @@ pacman -S xorg-server xorg-apps sddm plasma-desktop plasma-wayland-session
 
 useradd -m -G users,wheel,audio,video,plugdev,sddm -s /bin/bash connor
 printf " \n" > passwd connor
+printf "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
+
+
+
