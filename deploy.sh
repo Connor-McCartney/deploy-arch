47dbfd33cb3c1feaf62d0a267bf748ce59660116
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-20 00:00:43 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ DISK="/dev/sda"
 
 # BIOS
 printf "o\nn\n\n\n\n+8G\nn\n\n\n\n\nt\n1\n82\na\n2\nw\n" | fdisk $DISK  
-mkswap /dev/$DISK1
-mkfs.ext4 /dev/$DISK2
-mount /dev/$DISK2 /mnt
-swapon /dev/$DISK1
-pacstrap -K /mnt base linux linux-firmware NetworkManager nvim man-db man-pages texinfo
+mkswap "$DISK""1"
+mkfs.ext4 "$DISK""2"
+mount "$DISK""2" /mnt
+swapon "$DISK""1"
+pacstrap -K /mnt base linux linux-firmware networkmanager neovim man-db man-pages texinfo
 genfstab -U /mnt >> /mnt/etc/fstab
