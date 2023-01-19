45c56872f22f2974a2516de81fba38de1591e7c0
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-19 23:50:38 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 DISK="/dev/sda"
 
-
-printf "o\nn\n\n\n\n+8G\nn\n\n\n\n\nt\n1\n82\na\n2\nw\n" | fdisk $DISK  # BIOS
+# BIOS
+printf "o\nn\n\n\n\n+8G\nn\n\n\n\n\nt\n1\n82\na\n2\nw\n" | fdisk $DISK  
+mkswap /dev/$DISK1
+mkfs.ext4 /dev/$DISK2
