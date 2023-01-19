29aae19cb2e706b03ca3543bfb39ac77d6a6d0f0
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-19 23:57:01 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,7 @@ DISK="/dev/sda"
 printf "o\nn\n\n\n\n+8G\nn\n\n\n\n\nt\n1\n82\na\n2\nw\n" | fdisk $DISK  
 mkswap /dev/$DISK1
 mkfs.ext4 /dev/$DISK2
+mount /dev/$DISK2 /mnt
+swapon /dev/$DISK1
+pacstrap -K /mnt base linux linux-firmware NetworkManager nvim man-db man-pages texinfo
+genfstab -U /mnt >> /mnt/etc/fstab
