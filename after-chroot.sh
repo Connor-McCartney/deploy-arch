0378448d4558d0c9d0d9577ac39011acba3cebed
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-20 11:25:37 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -1,10 +1,24 @@
+DISK="/dev/sda"
 
-pacman -S xorg sddm konsole
+
+ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "arch" > /etc/hostname
+
+pacman -S xorg sddm grub konsole
 pacman -S plasma-desktop plasma-wayland-session 
 
 systemctl enable NetworkManager
 systemctl enable sddm.service
 
+grub-install $DISK # BIOS
+# grub-install --target=x86_64-efi --efi-directory=/boot # UEFI
+grub-mkconfig -o /boot/grub/grub.cfg
+
 useradd -m -G users,wheel,audio,video,sddm -s /bin/bash connor
-printf " \n" > passwd connor
+printf " \n \n" | passwd
+printf " \n \n" | passwd connor
 echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
