fc882f630cfdbf4bac5514ee7d026861dd7731f5
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-20 10:36:28 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -7,7 +7,4 @@ systemctl enable sddm.service
 
 useradd -m -G users,wheel,audio,video,plugdev,sddm -s /bin/bash connor
 printf " \n" > passwd connor
-printf "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
-
-
-
+echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
