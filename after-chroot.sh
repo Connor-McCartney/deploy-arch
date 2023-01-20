a818bad436a2eb090ba59463985e3b7c36fb924d
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-20 21:12:27 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -8,8 +8,13 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
-pacman -S --noconfirm xorg sddm grub konsole
+pacman -S --noconfirm xorg sddm grub konsole base-devel
 pacman -S --noconfirm plasma-desktop plasma-wayland-session 
+
+cd /tmp
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
 
 systemctl enable NetworkManager
 systemctl enable sddm.service
@@ -22,3 +27,5 @@ useradd -m -G users,wheel,audio,video,sddm -s /bin/bash connor
 printf " \n \n" | passwd
 printf " \n \n" | passwd connor
 echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
+
+rm /after-chroot.sh
