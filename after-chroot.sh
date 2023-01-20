bc03b9acd87be13da06fc1ce1a024f27548c7e53
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-20 11:08:00 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -5,6 +5,6 @@ pacman -S plasma-desktop plasma-wayland-session
 systemctl enable NetworkManager
 systemctl enable sddm.service
 
-useradd -m -G users,wheel,audio,video,plugdev,sddm -s /bin/bash connor
+useradd -m -G users,wheel,audio,video,sddm -s /bin/bash connor
 printf " \n" > passwd connor
 echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
