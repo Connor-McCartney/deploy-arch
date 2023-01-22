e5f42dfb252a4269bb2207ac7d31b5da68ce678f
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-23 06:18:09 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -25,20 +25,9 @@ printf " \n \n" | passwd
 printf " \n \n" | passwd connor
 echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
 
-su connor
-
-# Paru
-cd /tmp
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-
-printf "1\ny\n \n" | paru google-chrome
-printf "1\ny\n \n" | paru kwin-bismuth
-
-# Packer
-# https://github.com/wbthomason/packer.nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+cd /home/connor
+wget https://raw.githubusercontent.com/Connor-McCartney/deploy-arch/main/user.sh
+chmod +x user.sh
+/bin/su -c "/home/connor/user.sh" - connor
 
 rm /after-chroot.sh
