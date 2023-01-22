fdb614ec9731918fa959bddc60a13f0d2ec0e932
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-23 05:38:42 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -13,6 +13,21 @@ rustup default stable
 pacman -S --noconfirm base-devel xorg sddm grub konsole git npm wget spectacle discord sagemath
 pacman -S --noconfirm plasma-desktop plasma-wayland-session 
 
+systemctl enable NetworkManager
+systemctl enable sddm.service
+
+grub-install $DISK # BIOS
+# grub-install --target=x86_64-efi --efi-directory=/boot # UEFI
+grub-mkconfig -o /boot/grub/grub.cfg
+
+useradd -m -G users,wheel,audio,video,sddm -s /bin/bash connor
+printf " \n \n" | passwd
+printf " \n \n" | passwd connor
+echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
+
+su connor
+
+# Paru
 cd /tmp
 git clone https://aur.archlinux.org/paru.git
 cd paru
@@ -25,17 +40,5 @@ printf "1\ny\n \n" | paru kwin-bismuth
 # https://github.com/wbthomason/packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-
-systemctl enable NetworkManager
-systemctl enable sddm.service
-
-grub-install $DISK # BIOS
-# grub-install --target=x86_64-efi --efi-directory=/boot # UEFI
-grub-mkconfig -o /boot/grub/grub.cfg
-
-useradd -m -G users,wheel,audio,video,sddm -s /bin/bash connor
-printf " \n \n" | passwd
-printf " \n \n" | passwd connor
-echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
 
 rm /after-chroot.sh
