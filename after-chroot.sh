435566fc0a4a0293d3226daabef74d512649343e
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-23 05:48:12 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -8,7 +8,7 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
-pacman -Syu rustup
+pacman -Syu --noconfirm rustup
 rustup default stable
 pacman -S --noconfirm base-devel xorg sddm grub konsole git npm wget spectacle discord sagemath
 pacman -S --noconfirm plasma-desktop plasma-wayland-session 
