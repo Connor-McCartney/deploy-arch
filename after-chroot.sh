f423f811e396b8c6527f3c4e0c6d61cadd41d20e
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-23 05:06:54 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -20,6 +20,11 @@ makepkg -si
 printf "1\ny\n \n" | paru google-chrome
 printf "1\ny\n \n" | paru kwin-bismuth
 
+# Packer
+# https://github.com/wbthomason/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
 systemctl enable NetworkManager
 systemctl enable sddm.service
 
