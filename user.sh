53ba0f41dd8892d7fcd1b3969ff354bc115a43e1
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-23 12:47:28 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -18,6 +18,7 @@ paru -S --noconfirm kwin-bismuth
 # Packer
 # https://github.com/wbthomason/packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+sleep 5
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
