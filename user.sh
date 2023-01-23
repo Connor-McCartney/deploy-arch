ed300ebd2f380c30b56949c0ab8507008760db21
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-23 18:14:16 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -14,11 +14,12 @@ makepkg -si --noconfirm
 
 paru -S --noconfirm google-chrome
 paru -S --noconfirm kwin-bismuth-bin
+paru -S --noconfirm onlyoffice-bin
 
 # Packer
 # https://github.com/wbthomason/packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-#nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 sudo echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
 rm /home/connor/user.sh
