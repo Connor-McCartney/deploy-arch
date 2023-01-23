6df3b1d6e1ec1f9b5281b337c7c4e08e6004fbf7
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-23 18:39:25 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -9,7 +9,7 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
 pacman -Syu --noconfirm rustup
-pacman -S --noconfirm neovim man-db man-pages texinfo sudo base-devel xorg sddm grub vlc htop git npm wget keepassxc obsidian discord sagemath ttc-iosevka
+pacman -S --noconfirm networkmanager neovim man-db man-pages texinfo sudo base-devel xorg sddm grub vlc htop git npm wget keepassxc obsidian discord sagemath ttc-iosevka
 # https://archlinux.org/groups/x86_64/plasma/
 pacman -S --noconfirm plasma-desktop plasma-wayland-session plasma-nm plasma-pa dolphin konsole spectacle gwenview ark okular kwalletmanager
 
